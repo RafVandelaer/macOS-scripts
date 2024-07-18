eb50f631f4b83e4168a2496610f79e1e7e05fac5
Raf <rafman.rock@hotmail.com>
2024-07-18 12:55:51 +0200

    await dock for ADE enrollments

--- Intune-fundamentals/temp-working/fund-install-new.zsh
+++ Intune-fundamentals/temp-working/fund-install-new.zsh
@@ -93,8 +93,13 @@ scriptURL="https://raw.githubusercontent.com/Lab9Pro-AL/Intune/main/auto-app-upd
 mkdir $dir
 
 main() {
-    #Main function of this script, here is where the magic happens
-
+    #Main function of this script, this is where the magic happens
+	until ps aux | grep /System/Library/CoreServices/Dock.app/Contents/MacOS/Dock | grep -v grep &>/dev/null; do
+		delay=$(( $RANDOM % 50 + 10 ))
+		echo "$(date) |  + Dock not running, waiting [$delay] seconds"
+		sleep $delay
+	done
+	logging "Dock is here, lets carry on"
 	#checking if first run, if so we deploy all software and run DEPnotify
 	# Installs the latest release of Installomator from Github
     if [ -f $firstrun ]; then
