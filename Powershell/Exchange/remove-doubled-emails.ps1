e94cbb4845d05ada52732ca7d6a1fd15b21c7997
Raf <rafman.rock@hotmail.com>
2024-11-28 15:37:28 +0100

    added script to check for double emails and move them to a folder

---
+++ Powershell/Exchange/remove-doubled-emails.ps1
@@ -0,0 +1,46 @@
+# Define mailbox and target folder
+$mailbox = "user@example.com"
+$targetFolderName = "Double check"
+
+# Load Exchange Online PowerShell module
+Import-Module ExchangeOnlineManagement
+
+# Connect to Exchange Online
+Connect-ExchangeOnline -UserPrincipalName $mailbox -ShowProgress $true
+
+# Get mailbox folders
+$folders = Get-MailboxFolderStatistics -Identity $mailbox | Where-Object { $_.FolderType -eq "User Created" }
+
+# Check and create 'Double check' folder if it doesn't exist
+$targetFolder = Get-MailboxFolderStatistics -Identity $mailbox -FolderScope "All" | Where-Object { $_.Name -eq $targetFolderName }
+if (-not $targetFolder) {
+    New-MailboxFolder -Identity "$mailbox:\$targetFolderName"
+    Write-Output "Created folder: $targetFolderName"
+} else {
+    Write-Output "Folder already exists: $targetFolderName"
+}
+
+# Iterate through folders and find duplicate emails
+foreach ($folder in $folders) {
+    $folderPath = $folder.FolderPath -replace '/', '\'
+    $emails = Get-Content -Path "$mailbox\$folderPath"
+
+    Write-Output "Checking folder: $folderPath"
+
+    $emailHashes = @{}
+    foreach ($email in $emails) {
+        $hash = $email.Body.GetHashCode()
+
+        if ($emailHashes.ContainsKey($hash)) {
+            # Move duplicate email to 'Double check' folder
+            $email | Move-MailboxItem -DestinationFolder "$mailbox:\$targetFolderName"
+            Write-Output "Duplicate email moved to 'Double check' folder: $($email.Subject)"
+        } else {
+            $emailHashes[$hash] = $email
+        }
+    }
+}
+
+# Disconnect from Exchange Online
+Disconnect-ExchangeOnline -Confirm:$false
+Write-Output "Disconnected from Exchange Online"
